2f97bc5841829db481e2b4852dd71080d9cbb760
Fuzionfox04 <prashannaa.rajann@gmail.com>
2023-07-05 11:26:55 +0530

    bbb

--- ghost-ui/src/components/LiftButtons.jsx
+++ ghost-ui/src/components/LiftButtons.jsx
@@ -1,74 +1,80 @@
 import React, { useEffect, useState } from "react";
 
-function LiftButtons({style, setStyle, setDoor}) {
-
-  
-  let currentFloor = 1;
-
-function moveElevator(floor) {
-   // Assuming each floor has a height of 100px
-  const targetPosition = (floor - 1) * 130;
-  setStyle(
-    {
+function LiftButtons({ style, setStyle, setDoor }) {
+  function moveElevator(floor) {
+    // Assuming each floor has a height of 100px
+    const targetPosition = (floor + 1) * 130;
+    setStyle({
       transform: `translateY(${targetPosition}px)`,
-      transitionDuration: "2s",
-    }
+      transitionDuration: "1s",
+    });
 
-  );
- 
-  currentFloor = floor;
-  setTimeout(() => {
-    opendoor();
+    currentFloor = floor;
     setTimeout(() => {
-      closedoor();
-    },3000 
-    );
-  },3000 
-  );
-}
-  function opendoor(){
-    setDoor(true);
-
+      opendoor();
+      setTimeout(() => {
+        closedoor();
+      }, 5000);
+    }, 1000);
   }
-  function closedoor(){
+  function opendoor() {
+    setDoor(true);
+  }
+  function closedoor() {
     setDoor(false);
   }
-  useEffect(()=>{
-    var buttons=document.querySelectorAll('.floor button');
+  useEffect(() => {
+    var buttons = document.querySelectorAll(".floor button");
     buttons.forEach((button) => {
-     button.addEventListener('click', function () {
-       console.log(button);
-     })});
+      button.addEventListener("click", function () {
+        console.log(button);
+      });
+    });
+  }, []);
 
-  },[])
-
-
-  return (<div>
-
+  return (
+    <div>
       <div class="floor">
-        <button onClick={()=>{
-          moveElevator(1);
-        }}>Floor 4</button>
-        <button onClick={()=>{
-          moveElevator(2);
-        }}>Floor 3</button>
-        <button onClick={()=>{
-          moveElevator(3);
-        }}>Floor 2</button>
-        <button onClick={()=>{
-          moveElevator(4);
-        }}>Floor 1</button>
-        <button onClick={()=>{
-          moveElevator(5);
-        }}>Floor G</button>
-        <button onClick={
-          opendoor
-        }>open</button>
-        <button onClick={
-          closedoor
-        }>close</button>
+        <button
+          onClick={() => {
+            moveElevator(0);
+          }}
+        >
+          Floor 4
+        </button>
+        <button
+          onClick={() => {
+            moveElevator(1);
+          }}
+        >
+          Floor 3
+        </button>
+        <button
+          onClick={() => {
+            moveElevator(2);
+          }}
+        >
+          Floor 2
+        </button>
+        <button
+          onClick={() => {
+            moveElevator(3);
+          }}
+        >
+          Floor 1
+        </button>
+        <button
+          onClick={() => {
+            moveElevator(4);
+          }}
+        >
+          Floor G
+        </button>
+        <button onClick={opendoor}>open</button>
+        <button onClick={closedoor}>close</button>
       </div>
-  </div>);
+    </div>
+  );
 }
 
 export default LiftButtons;

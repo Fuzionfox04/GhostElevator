8fd47545a74aa551f893445604bc763f669479c6
Fuzionfox04 <prashannaa.rajann@gmail.com>
2023-07-06 21:50:04 +0530

    addedd

--- ghost-ui/src/pages/Log.jsx
+++ ghost-ui/src/pages/Log.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Log() {
-  return <div>Log</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <button class="btn-floor" onClick={() => navigate("/elevator")}>
+        elevator
+      </button>
+    </div>
+  );
 }
 
 export default Log;
